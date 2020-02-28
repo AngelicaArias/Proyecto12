8b7bc419fb7faae99a6d75d929a6e2b33b58edf6
Angie <al221611547@gmail.com>
2020-02-28 16:59:05 -0600

    vercion3

---
+++ ejemplo2.dart
@@ -0,0 +1,76 @@
+main() {
+
+  Animal ani = Animal (4, true, 'gato');
+  ani.respirar();
+  ani.comer('pedigri');
+ 
+  Perro per = Perro ('pelusa', 2, 4, true, 'perro');
+  per.ladrar('tania');
+  
+  
+  Comida com = Comida ('croquetas');
+  com.nombrec = "croquetas";
+  per.hacerPopo(com);
+  
+  Popo po = Popo (3.5);
+  po.peso = 3.5;
+  
+  
+}
+
+class Animal{
+  int numPatas;
+  bool patas;
+  String nombreespecie;
+  
+  Animal(this.numPatas, this.patas, this.nombreespecie);
+  
+  void respirar(){
+    print('El animal $nombreespecie respira');
+  }
+  
+  String comer(String croquetas){
+    return "comer";
+    
+  }
+  
+}
+
+
+class Perro extends Animal{
+  String nombre;
+  int edad;
+  
+  Perro (String nombre, int edad, int numPatas, bool patas, String nombreespecie) : super (numPatas, patas, nombreespecie){
+  
+     this.edad = edad;
+     this.nombre = nombre;    
+     
+   } 
+  
+  String ladrar(String voz){
+    return 'guau';
+  
+  }
+  
+  Popo hacerPopo(Comida com){
+    Popo pop = Popo(.500);
+    print(pop.peso);
+    return pop;
+    
+  }
+  
+}
+
+class Comida{
+  String nombrec;
+  
+  Comida(this.nombrec);
+  
+  
+}
+
+class Popo{
+  double peso;
+  Popo(this.peso);
+}
